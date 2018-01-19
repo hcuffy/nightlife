986c4f227a4911b9ee5ca36b5729b7267a8826a4
Henry Cuffy <hcuffy_5@hotmail.com>
2018-01-19 22:29:47 +0100

    Adding value to going button.

--- controllers/search.js
+++ controllers/search.js
@@ -2,23 +2,6 @@ const yelp = require('yelp-fusion');
 const client = yelp.client(process.env.YELP_API);
 const Attending = require('../models/attend')
 
-function getAttendence(yelpId, callback) {
-
-  Attending.find({
-    place: yelpId
-  }, function (err, data) {
-    if (err)
-      return err;
-    let len = data.length;
-    if (len === 0) {
-      return callback(data);
-    } else {
-      return callback (null, data);
-    }
-  });
-}
-
-exports.getAttendence = getAttendence;
 
 exports.getSearch = (req, res, next) => {
   const {phrase, place} = req.query;
@@ -26,19 +9,37 @@ exports.getSearch = (req, res, next) => {
     let allLocations = res1.jsonBody.businesses;
     let listOfPlaces = [];
     for (let i = 0; i < allLocations.length; i++) {
-      let going = getAttendence(allLocations[i].id, next);
-       console.log(going);
       let entry = {
         name: allLocations[i].name,
         id: allLocations[i].id,
         image: allLocations[i].image_url,
         rating: allLocations[i].rating,
         address: allLocations[i].location.display_address,
-        contact: allLocations[i].phone
+        contact: allLocations[i].phone,
+        going: null
+
       }
+
       listOfPlaces.push(entry);
     }
-    res.render('locations', {listOfPlaces})
+    Attending.find({}, (err, data) => {
+      if (err)
+        return next(err)
+      for (var i = 0; i < data.length; i++) {
+        let place = data[i].place;
+        for (var j = 0; j < listOfPlaces.length; j++) {
+          if (place === listOfPlaces[j].id && Number.isInteger(data[i].attending)) {
+            listOfPlaces[j].going = data[i].attending;
+            console.log(listOfPlaces[j].going);
+          } else if(isNaN(data[i].attending)){
+            listOfPlaces[j].going = 0;
+            console.log(listOfPlaces[j].going);
+          }
+        }
+      }
+      res.render('locations', {listOfPlaces});
+    });
+
   }).catch(e => {
     console.log(e);
   });
@@ -81,6 +82,5 @@ exports.addAttendence = (req, res, next) => {
     }
 
   });
-  return;
 
 }
