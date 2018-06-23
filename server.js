c54038fd1a8dab553990d1d17dc876c4fe7c8dfa
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 17:38:51 +0200

    Update folder structure

--- server.js
+++ server.js
@@ -13,24 +13,26 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nightlife');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json('*/*'));
 app.use(logger('combined'));
 
-app.use(session({secret: 'nightlife', saveUninitialized: true, resave: true}));
+app.use(
+	session({ secret: 'nightlife', saveUninitialized: true, resave: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('*', function(req, res, next) {
-  res.locals.user = req.user || null;
-  res.locals.error = null;
-  next();
+	res.locals.user = req.user || null;
+	res.locals.error = null;
+	next();
 });
 
 const routes = require('./routes');
 app.use('/', routes);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Express server running on port', port)
+	console.log('Express server running on port', port);
 });
